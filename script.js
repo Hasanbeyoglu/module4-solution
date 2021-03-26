fa31b4c76e7f1996fc13ed5b5dc51cc7f7331526
Ertugrul Hasanbeyoglu <ertugrulhasanbeyoglu@gmail.com>
2021-03-27 02:49:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var names = ["Jason", "William", "John", "Sam", "Anna", "George", "Sandra", "Jim" ]
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"]
 
 for (var i=0; i < names.length; i++) {
     
